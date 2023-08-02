dd03a58600aa34d4c273cd676818ffc1a3ea2da6
Jim Grimes <jimgrimes86@gmail.com>
2023-08-02 09:40:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const gifters = ["Adam", "Brian", "Charles"];
+
+let eventName = "birthday";
+
+const cardLog = []
+
+function writeCards(gifters, eventName) {
+  for (let i = 0; i < gifters.length; i++) {
+    cardLog.push(`Thank you, ${gifters[i]}, for the wonderful ${eventName} gift!`)
+  }
+  
+  return cardLog;
+}
+
+function countDown(countNumber) {
+    while (countNumber >= 0) {
+      console.log(countNumber--);
+    }
+  }
+  
